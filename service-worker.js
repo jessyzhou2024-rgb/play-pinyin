6a0f405746089066eb9ccfbe5ad268ea71bbd0f2
jessy zhou <jessy.zhou.2024@gmail.com>
2026-06-28 19:14:12 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "pinyin-play-town-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./docs.html",
+  "./GAME_DOCUMENTATION.md",
+  "./README.md",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
